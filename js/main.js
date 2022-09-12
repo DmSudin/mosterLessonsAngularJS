bd56926dc36425e3358e19a9d2df7b9431df1c54
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-12 22:52:52 +0300

    lesson 3 - sharing data between controllers

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-angular.module('app', []).controller('myCtrl', function($scope) {
-    $scope.hello = 55;
-    $scope.myBook = 'AngularJS'
+let app = angular.module('app', []);
+
+app.controller('firstCtrl', function($scope, myFactory) {
+    $scope.myFactory = myFactory;
+    console.log('firstCtrl')
+    $scope.hello = 'hello world';
+});
+
+app.controller('secondCtrl', function($scope, myFactory) {
+    $scope.myFactory = myFactory;
+    console.log('secondCtrl')
+    $scope.hello = 'hello world';
+});
+
+app.factory('myFactory', function() {
+    return {
+        hello: 'hello world'
+    };
 });
