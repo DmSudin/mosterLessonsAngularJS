3ecb7f7c14e5d9b2c0271b8c2669d0e7b160dfa1
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-13 21:54:54 +0300

    lesson 9 - restrict

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 let app = angular.module('app', []);
 
-app.controller('mainCtrl', function($scope) {
-    $scope.money1 = '1.22$';
-    $scope.money2 = '$2.33';
-    $scope.money3 = '4.33';
-});
-
-app.filter('moneyFilter', function() {
-    return function(str) {
-        let lastChar = str.slice(-1),
-            firstChar = str.slice(0, 1),
-            slicedPart;
-        if (lastChar === '$') {
-            slicedPart = str.slice(0, str.length - 1);
-            return '$' + slicedPart;
-        } else if (firstChar !== '$') {
-            return '$' + str;
+app.directive('fooBar', function() {
+    return {
+        restrict: 'EAMC',
+        link: function() {
+            console.log('fooBar');
         }
-        return str;
     };
+
 });
