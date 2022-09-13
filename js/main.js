2564e351a8b05c9f125be4b328dfde10718c540a
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-13 22:16:25 +0300

    lesson 10 - template in directive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 let app = angular.module('app', []);
 
 app.directive('fooBar', function() {
+    let bookmarks = [
+        {
+            id: 1,
+            name: 'Angular JS'
+        },
+        {
+            id: 2,
+            name: 'Ember JS'
+        },
+        {
+            id: 3,
+            name: 'React JS'
+        },
+    ];
+
     return {
-        restrict: 'EAMC',
-        link: function() {
-            console.log('fooBar');
+        template: `<div ng-repeat="bookmark in myBookmarks">{{bookmark.name}}</div>`,        
+        link: function(scope, element, attrs) {
+            scope.name = 'user';
+            scope.myBookmarks = bookmarks;
         }
     };
-
 });
