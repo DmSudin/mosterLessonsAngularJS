efb8e9272f390bdd5c1968426c4e94efb906f47a
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-18 21:32:34 +0300

    lesson 13 - template cache

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 let app = angular.module('app', []);
 
+app.run(function($templateCache) {
+    $templateCache.put('bookmarks.html', "<div ng-repeat='bookmark in bookmarks'>{{bookmark.name}}</div>")
+
+});
+
 app.controller('mainCtrl', function($scope) {
 });
 
-app.directive('fooBar', function() {
+app.directive('fooBar', function($templateCache) {
     let bookmarks = [
         {
             id: 1,
@@ -19,7 +24,8 @@ app.directive('fooBar', function() {
         replace: false,
         templateUrl: 'bookmarks.html',
         link: function(scope, element, attrs) {
-            scope.bookmarks = bookmarks;            
+            scope.bookmarks = bookmarks;
+            console.error($templateCache.info());
         }
     };
 });
