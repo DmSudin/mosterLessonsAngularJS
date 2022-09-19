2baa9e1e76cf67445346ba808feb13d739108688
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-19 21:03:45 +0300

    lesson 18 - http requests

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 let app = angular.module('app', []);
 
+app.controller('mainCtrl', function($http, $scope) {
+    $http.get('http://localhost:3001/books')
+        .success(function(result) {
+            console.error('success', result);
+            $scope.books = result;
+        })
+        .error(function(result){
+            console.error('error');
+        });
 
-
-
-app.directive('wrapIn', function($templateCache) {
-
-    return {
-        transclude: 'element',
-        link: function(scope, element, attrs, ctrl, transclude) {
-            let template = $templateCache.get(attrs.wrapIn);
-            let templateElement = angular.element(template);
-            transclude(scope, function(clone) {
-                element.after(templateElement.append(clone));
-            });
-        },
-    };
+        $scope.addBook = function(book) {
+            console.error(book);
+            $http.post('http://localhost:3001/books', book)
+                .success(function(result) {
+                    console.error('saved');
+                    $scope.books.push(book);
+                    $scope.book = null;
+                })
+                .error(function(result) {
+                    console.error('error while saving');
+                    console.error(result);
+                })
+        }
 });
