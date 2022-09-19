a36bc5ab207ced70b306147a30a05899f419b251
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-19 21:25:32 +0300

    lesson 19 - mock data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-let app = angular.module('app', []);
+let app = angular.module('app', ['ngMockE2E']);
+app.run(function($httpBackend){
+    let books = [
+        {
+            name: 'AngularJS'
+        },
+        {
+            name: 'EmberJS'
+        },
+        {
+            name: 'ReactJS'
+        }
+    ];
+    $httpBackend.whenGET('http://localhost:3001/books').respond(200, books);
+    $httpBackend.whenPOST('http://localhost:3001/books').respond(function(method, url, data) {
+        let result = JSON.parse(data);
+        books.push(result);
+        return [200, result];
+    });
+});
 
 app.controller('mainCtrl', function($http, $scope) {
     $http.get('http://localhost:3001/books')
