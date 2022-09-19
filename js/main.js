ec4c1309b35401463e09163dba44d605400019ee
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-19 21:55:29 +0300

    lesson 20 - compile function in directives

--- js/main.js
+++ js/main.js
@@ -1,45 +1,20 @@
-let app = angular.module('app', ['ngMockE2E']);
-app.run(function($httpBackend){
-    let books = [
-        {
-            name: 'AngularJS'
-        },
-        {
-            name: 'EmberJS'
-        },
-        {
-            name: 'ReactJS'
-        }
-    ];
-    $httpBackend.whenGET('http://localhost:3001/books').respond(200, books);
-    $httpBackend.whenPOST('http://localhost:3001/books').respond(function(method, url, data) {
-        let result = JSON.parse(data);
-        books.push(result);
-        return [200, result];
-    });
-});
 
-app.controller('mainCtrl', function($http, $scope) {
-    $http.get('http://localhost:3001/books')
-        .success(function(result) {
-            console.error('success', result);
-            $scope.books = result;
-        })
-        .error(function(result){
-            console.error('error');
-        });
+let app = angular.module('app', []);
 
-        $scope.addBook = function(book) {
-            console.error(book);
-            $http.post('http://localhost:3001/books', book)
-                .success(function(result) {
-                    console.error('saved');
-                    $scope.books.push(book);
-                    $scope.book = null;
-                })
-                .error(function(result) {
-                    console.error('error while saving');
-                    console.error(result);
-                })
+app.directive('uiSource', function(){
+    return {
+        compile: function(element) {
+            let escape = function(content) {
+                return content.replace(/\</g, '&lt;')
+                                .replace(/\>/g, '&gt;')
+
+            };
+
+            let pre = angular.element("<pre class='prettyprint'></pre>");
+            let pretty = prettyPrintOne(escape(element.html()));
+            console.error(pretty);
+            pre.append(pretty);
+            element.replaceWith(pre);
         }
+    };
 });
