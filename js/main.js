8b3acb872a5a03c1646743adb0552007dbbae12a
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-09-20 15:28:49 +0300

    lesson 23 - ng-route & route-params

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ app.config(function($routeProvider){
             controller: 'homeCtrl'
         })
         .when('/posts', {
-            template: '<h1>Posts</h1>'
+            templateUrl: 'posts.html',
+            controller: 'postsCtrl'
+        })
+        .when('/posts/:postId', {
+            templateUrl: 'post.html',
+            controller: 'postCtrl'
+        })
+        .otherwise({
+            template: '404 - no such page'
         })
 });
 
@@ -16,4 +24,31 @@ app.controller('homeCtrl', function($scope) {
     $scope.model = {
         message: 'this is homepage'
     };
+});
+
+app.controller('postCtrl', function($scope, $routeParams, postsFactory) {
+    let postId = Number($routeParams.postId);
+    $scope.post = _.findWhere(postsFactory, {id: postId});
+
+});
+
+app.controller('postsCtrl', function($scope, postsFactory) {
+    $scope.posts = postsFactory;
+});
+
+app.factory('postsFactory', function() {
+    return [
+        {
+            id: 1,
+            name: 'post one'
+        },
+        {
+            id: 2,
+            name: 'post two'
+        },
+        {
+            id: 3,
+            name: 'post three'
+        },
+    ];
 });
